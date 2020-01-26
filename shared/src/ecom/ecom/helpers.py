b1ca854b785aa6ca9a5194cff6f809b285b22f17
Nicolas Moutschen <moutschn@amazon.com>
2020-01-26 13:23:28 +0100

    shared: add ddb_to_event in ecom.helpers

--- shared/src/ecom/ecom/helpers.py
+++ shared/src/ecom/ecom/helpers.py
@@ -7,6 +7,7 @@ from datetime import datetime, date
 from decimal import Decimal
 import json
 from typing import Dict, Union
+from boto3.dynamodb.types import TypeDeserializer
 
 
 __all__ = ["Encoder", "message"]
@@ -25,6 +26,65 @@ class Encoder(json.JSONEncoder):
                 return float(o)
             return int(o)
         return super(Encoder, self).default(o)
+
+
+def ddb_to_event(ddb_record: dict, event_bus_name: str, source: str,
+                 object_type: str, resource_key: str) -> dict:
+    """
+    Transforms a DynamoDB Streams record into an EventBridge event
+
+    For this function to works, you need to have a StreamViewType of
+    NEW_AND_OLD_IMAGES.
+    """
+
+    deserialize = TypeDeserializer().deserialize
+
+    event = {
+        "Time": datetime.now(),
+        "Source": source,
+        "Resources": [
+            str(deserialize(ddb_record["dynamodb"]["Keys"][resource_key]))
+        ],
+        "EventBusName": event_bus_name
+    }
+
+    # Created event
+    if ddb_record["eventName"].upper() == "INSERT":
+        event["DetailType"] = "{}Created".format(object_type)
+        event["Detail"] = json.dumps({
+            k: deserialize(v)
+            for k, v
+            in ddb_record["dynamodb"]["NewImage"].items()
+        }, cls=Encoder)
+
+    # Deleted event
+    elif ddb_record["eventName"].upper() == "REMOVE":
+        event["DetailType"] = "{}Deleted".format(object_type)
+        event["Detail"] = json.dumps({
+            k: deserialize(v)
+            for k, v
+            in ddb_record["dynamodb"]["OldImage"].items()
+        }, cls=Encoder)
+
+    elif ddb_record["eventName"].upper() == "MODIFY":
+        event["DetailType"] = "{}Modified".format(object_type)
+        event["Detail"] = json.dumps({
+            "new": {
+                k: deserialize(v)
+                for k, v
+                in ddb_record["dynamodb"]["NewImage"].items()
+            },
+            "old": {
+                k: deserialize(v)
+                for k, v
+                in ddb_record["dynamodb"]["OldImage"].items()
+            }
+        }, cls=Encoder)
+
+    else:
+        raise ValueError("Wrong eventName value for DynamoDB event: {}".format(ddb_record["eventName"]))
+
+    return event
 
 
 def message(msg: Union[dict, str], status_code: int = 200) -> Dict[str, Union[int, str]]:
--- shared/src/ecom/tests/test_helpers.py
+++ shared/src/ecom/tests/test_helpers.py
@@ -1,6 +1,8 @@
 import datetime
 import decimal
 import json
+import uuid
+import pytest
 from ecom import helpers # pylint: disable=import-error
 
 
@@ -14,6 +16,161 @@ def test_encoder(lambda_module):
     assert isinstance(encoder.default(decimal.Decimal(10.5)), float)
     assert isinstance(encoder.default(decimal.Decimal(10)), int)
     assert isinstance(encoder.default(datetime.datetime.now()), str)
+
+
+def test_ddb_to_event_insert():
+    """
+    Test ddb_to_event() with an INSERT record
+    """
+
+    event_bus_name = "EVENT_BUS_NAME"
+    source = "SOURCE"
+    object_type = "Object"
+    resource_key = "pk"
+
+    record = {
+        "awsRegion": "eu-west-1",
+        "dynamodb": {
+            "Keys": {
+                "pk": {"N": "123"},
+                "sk": {"N": "456"}
+            },
+            "NewImage": {
+                "SomeKey": {"S": "SomeValue"},
+                "SomeOtherKey": {"N": "123456"}
+            },
+            "SequenceNumber": "1234567890123456789012345",
+            "SizeBytes": 123,
+            "StreamViewType": "NEW_AND_OLD_IMAGES"
+        },
+        "eventID": str(uuid.uuid4()),
+        "eventName": "INSERT",
+        "eventSource": "aws:dynamodb",
+        "eventVersion": "1.0"
+    }
+
+    event = {
+        "Source": source,
+        "Resources": ["123"],
+        "DetailType": "ObjectCreated",
+        "Detail": {"SomeKey": "SomeValue", "SomeOtherKey": 123456},
+        "EventBusName": event_bus_name
+    }
+
+    retval = helpers.ddb_to_event(record, event_bus_name, source, object_type, resource_key)
+
+    for key, value in event.items():
+        assert key in retval
+
+        if key == "Detail":
+            assert json.loads(value) == event[key]
+        else:
+            assert value == event[key]
+
+
+def test_ddb_to_event_remove():
+    """
+    Test ddb_to_event() with a REMOVE record
+    """
+
+    event_bus_name = "EVENT_BUS_NAME"
+    source = "SOURCE"
+    object_type = "Object"
+    resource_key = "pk"
+
+    record = {
+        "awsRegion": "eu-west-1",
+        "dynamodb": {
+            "Keys": {
+                "pk": {"N": "123"},
+                "sk": {"N": "456"}
+            },
+            "OldImage": {
+                "SomeKey": {"S": "SomeValue"},
+                "SomeOtherKey": {"N": "123456"}
+            },
+            "SequenceNumber": "1234567890123456789012345",
+            "SizeBytes": 123,
+            "StreamViewType": "NEW_AND_OLD_IMAGES"
+        },
+        "eventID": str(uuid.uuid4()),
+        "eventName": "REMOVE",
+        "eventSource": "aws:dynamodb",
+        "eventVersion": "1.0"
+    }
+
+    event = {
+        "Source": source,
+        "Resources": ["123"],
+        "DetailType": "ObjectDeleted",
+        "Detail": {"SomeKey": "SomeValue", "SomeOtherKey": 123456},
+        "EventBusName": event_bus_name
+    }
+
+    retval = helpers.ddb_to_event(record, event_bus_name, source, object_type, resource_key)
+
+    for key, value in event.items():
+        assert key in retval
+
+        if key == "Detail":
+            assert json.loads(value) == event[key]
+        else:
+            assert value == event[key]
+
+
+def test_ddb_to_event_modify():
+    """
+    Test ddb_to_event() with a MODIFY record
+    """
+
+    event_bus_name = "EVENT_BUS_NAME"
+    source = "SOURCE"
+    object_type = "Object"
+    resource_key = "pk"
+
+    record = {
+        "awsRegion": "eu-west-1",
+        "dynamodb": {
+            "Keys": {
+                "pk": {"N": "123"},
+                "sk": {"N": "456"}
+            },
+            "NewImage": {
+                "SomeKey": {"S": "SomeValue"}
+            },
+            "OldImage": {
+                "SomeOtherKey": {"N": "123456"}
+            },
+            "SequenceNumber": "1234567890123456789012345",
+            "SizeBytes": 123,
+            "StreamViewType": "NEW_AND_OLD_IMAGES"
+        },
+        "eventID": str(uuid.uuid4()),
+        "eventName": "REMOVE",
+        "eventSource": "aws:dynamodb",
+        "eventVersion": "1.0"
+    }
+
+    event = {
+        "Source": source,
+        "Resources": ["123"],
+        "DetailType": "ObjectModified",
+        "Detail": {
+            "new": {"SomeKey": "SomeValue"},
+            "old": {"SomeOtherKey": 123456}
+        },
+        "EventBusName": event_bus_name
+    }
+
+    retval = helpers.ddb_to_event(record, event_bus_name, source, object_type, resource_key)
+
+    for key, value in event.items():
+        assert key in retval
+
+        if key == "Detail":
+            assert json.loads(value) == event[key]
+        else:
+            assert value == event[key]
 
 
 def test_message_string():
