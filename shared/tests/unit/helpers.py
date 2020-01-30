cd5c253e3bcf795eaa9882ad79e4953ec28694fe
Nicolas Moutschen <moutschn@amazon.com>
2020-01-30 13:20:11 +0100

    shared: add fixtures and helpers for unit tests

--- shared/tests/unit/helpers.py
+++ shared/tests/unit/helpers.py
@@ -1,5 +1,5 @@
 import json
-from typing import Optional
+from typing import List, Optional, Union
 from boto3.dynamodb.types import TypeSerializer
 from botocore import stub
 
@@ -52,58 +52,96 @@ def compare_event(a: dict, b: dict):
 
 
 def mock_table(
-        ddb_table, action: str, key: str,
-        item: Optional[dict]=None,
+        ddb_table,
+        action: str,
+        keys: Union[str, List[str]],
+        items: Optional[Union[dict, List[dict]]]=None,
+        table_name: Optional[str]=None,
         expected_params: Optional[dict]=None,
-        response: Optional[dict]=None) -> stub.Stubber:
+        response: Optional[dict]=None
+    ) -> stub.Stubber:
     """
     Mock a DynamoDB table
     """
 
-    SUPPORTED_ACTIONS = ["get_item", "query", "scan"]
+    SUPPORTED_ACTIONS = [
+        "delete_item", "get_item", "put_item",
+        "query", "scan", "batch_write_item"
+    ]
 
     if action not in SUPPORTED_ACTIONS:
         raise ValueError("DynamoDB action {} is not supported".format(action))
 
-    table = stub.Stubber(ddb_table.meta.client)
+    if isinstance(ddb_table, stub.Stubber):
+        table = ddb_table
+    else:
+        table = stub.Stubber(ddb_table.meta.client)
 
-    if action == "get_item":
+    if isinstance(keys, str):
+        keys = [keys]
+
+    if isinstance(items, dict):
+        items = [items]
+
+    # Key-based, one item
+    if action in ["delete_item", "get_item"]:
+        assert items is None or len(items) == 1
         response = response or {
             "ConsumedCapacity": {}
         }
         expected_params = expected_params or {
-            "TableName": ddb_table.name,
-            "Key": {key: stub.ANY}
+            "TableName": table_name or ddb_table.name,
+            "Key": {key: stub.ANY for key in keys}
         }
-        if item is not None:
-            response["Item"] = {k: TypeSerializer().serialize(v) for k, v in item.items()}
-            expected_params["Key"] = {key: item[key]}
-        table.add_response("get_item", response, expected_params)
+        if items is not None and action == "get_item":
+            response["Item"] = {k: TypeSerializer().serialize(v) for k, v in items[0].items()}
+        if items is not None:
+            expected_params["Key"] = {key: items[0][key] for key in keys}
+        table.add_response(action, response, expected_params)
 
-    if action == "query":
+    # Item-based, one item
+    elif action in ["put_item"]:
+        assert items is None or len(items) == 1
         response = response or {
             "ConsumedCapacity": {}
         }
         expected_params = expected_params or {
-            "TableName": ddb_table.name,
-            "KeyConditionExpression": stub.ANY,
+            "TableName": table_name or ddb_table.name,
+            "Item": stub.ANY
+        }
+        if items is not None:
+            expected_params["Item"] = items[0]
+        table.add_response(action, response, expected_params)
+
+    # Key-based, multiple items
+    elif action in ["query", "scan"]:
+        response = response or {
+            "ConsumedCapacity": {}
+        }
+        expected_params = expected_params or {
+            "TableName": table_name or ddb_table.name,
             "Limit": stub.ANY
         }
-        if item is not None:
-            response["Items"] = [{k: TypeSerializer().serialize(v) for k, v in item.items()}]
-        table.add_response("query", response, expected_params)
+        if action == "query" and "KeyConditionExpression" not in expected_params:
+            expected_params["KeyConditionExpression"] = stub.ANY
+        if items is not None:
+            response["Items"] = [{k: TypeSerializer().serialize(v) for k, v in item.items()} for item in items]
+        table.add_response(action, response, expected_params)
 
-    if action == "scan":
+    # Batch operations
+    elif action in ["batch_write_item"]:
         response = response or {
-            "ConsumedCapacity": {}
+            "ConsumedCapacity": [{} for _ in (items or [])],
+            "UnprocessedItems": {}
         }
         expected_params = expected_params or {
-            "TableName": ddb_table.name,
-            "Limit": stub.ANY
+            "RequestItems": {
+                table_name or ddb_table.name: stub.ANY
+            }
         }
-        if item is not None:
-            response["Items"] = [{k: TypeSerializer().serialize(v) for k, v in item.items()}]
-        table.add_response("scan", response, expected_params)
+        if items is not None:
+            expected_params["RequestItems"][table_name or ddb_table.name] = items
+        table.add_response(action, response, expected_params)
 
     table.activate()
 
