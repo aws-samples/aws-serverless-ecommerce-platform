d7e4f4f2636bec563e8d56648b48888e3e3b83a3
Nicolas Moutschen <moutschn@amazon.com>
2020-02-11 18:32:25 +0100

    warehouse: added table_update function

---
+++ warehouse/src/table_update/main.py
@@ -0,0 +1,141 @@
+"""
+TableUpdateFunction
+"""
+
+
+import datetime
+import json
+import os
+from typing import List, Optional
+from aws_lambda_powertools.tracing import Tracer
+from aws_lambda_powertools.logging import logger_setup, logger_inject_lambda_context
+import boto3
+from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.types import TypeDeserializer
+from ecom.helpers import Encoder #pylint: disable=import-error
+
+
+ENVIRONMENT = os.environ["ENVIRONMENT"]
+EVENT_BUS_NAME = os.environ["EVENT_BUS_NAME"]
+METADATA_KEY = os.environ["METADATA_KEY"]
+TABLE_NAME = os.environ["TABLE_NAME"]
+
+
+dynamodb = boto3.resource("dynamodb") # pylint: disable=invalid-name
+eventbridge = boto3.client("events") # pylint: disable=invalid-name
+table = dynamodb.Table(TABLE_NAME) # pylint: disable=invalid-name,no-member
+type_deserializer = TypeDeserializer() # pylint: disable=invalid-name
+logger = logger_setup() # pylint: disable=invalid-name
+tracer = Tracer() # pylint: disable=invalid-name
+
+
+@tracer.capture_method
+def send_events(events: List[dict]):
+    """
+    Send events to EventBridge
+    """
+
+    logger.info("Sending %d events to EventBridge", len(events))
+    eventbridge.put_events(Entries=events)
+
+
+@tracer.capture_method
+def parse_record(ddb_record: dict) -> Optional[dict]:
+    """
+    Parse a DynamoDB record into an EventBridge event
+    """
+
+    # Discard records that concern removed events, non-metadata items or items that are
+    # not in the COMPLETED status.
+    if (ddb_record["eventName"].upper() == "REMOVE"
+        or ddb_record["dynamodb"]["NewImage"]["productId"]["S"] != METADATA_KEY
+        or ddb_record["dynamodb"]["NewImage"]["status"]["S"] != "COMPLETED"):
+        return None
+
+    # Gather information
+    order_id = ddb_record["dynamodb"]["NewImage"]["orderId"]["S"]
+    products = get_products(order_id)
+
+    # Return event
+    return {
+        "Time": datetime.datetime.now(),
+        "Source": "ecommerce.warehouse",
+        "Resources": [order_id],
+        "EventBusName": EVENT_BUS_NAME,
+        "DetailType": "PackageCreated",
+        "Detail": json.dumps({
+            "orderId": order_id,
+            "products": products
+        }, cls=Encoder)
+    }
+
+@tracer.capture_method
+def get_products(order_id: str) -> List[dict]:
+    """
+    Retrieve products from the DynamoDB table
+    """
+
+    res = table.query(
+        KeyConditionExpression=Key("orderId").eq(order_id),
+        Limit=100
+    )
+    logger.info({
+        "message": "Retrieving {} products from order {}".format(
+            len(res.get("Items", [])), order_id
+        ),
+        "operation": "query",
+        "orderId": order_id
+    })
+    products = res.get("Items", [])
+
+    while res.get("LastEvaluatedKey", None) is not None:
+        res = table.query(
+            KeyConditionExpression=Key("orderId").eq(order_id),
+            ExclusiveStartKey=res["LastEvaluatedKey"],
+            Limit=100
+        )
+        logger.info({
+            "message": "Retrieving {} products from order {}".format(
+                len(res.get("Items", [])), order_id
+            ),
+            "operation": "query",
+            "orderId": order_id
+        })
+        products.extend(res.get("Items", []))
+
+    return products
+
+
+@logger_inject_lambda_context
+@tracer.capture_lambda_handler
+def handler(event, _):
+    """
+    Lambda function handler for Warehouse Table stream
+    """
+
+    logger.debug({
+        "message": "Input event",
+        "event": event
+    })
+
+    logger.debug({
+        "message": "Records received",
+        "records": event.get("Records", [])
+    })
+
+    # Parse events
+    events = [
+        parse_record(record)
+        for record in event.get("Records", [])
+    ]
+
+    # Filter None events
+    events = [event for event in events if event is not None]
+
+    logger.info("Received %d event(s)", len(events))
+    logger.debug({
+        "message": "Events processed from records",
+        "events": events
+    })
+
+    send_events(events)
---
+++ warehouse/tests/unit/test_table_update.py
@@ -0,0 +1,229 @@
+import copy
+import datetime
+import json
+import random
+import uuid
+from botocore import stub
+import pytest
+from fixtures import context, lambda_module, get_order, get_product # pylint: disable=import-error
+from helpers import compare_event, mock_table # pylint: disable=import-error,no-name-in-module
+
+
+METADATA_KEY = "__metadata"
+
+
+lambda_module = pytest.fixture(scope="module", params=[{
+    "function_dir": "table_update",
+    "module_name": "main",
+    "environ": {
+        "ENVIRONMENT": "test",
+        "METADATA_KEY": METADATA_KEY,
+        "TABLE_NAME": "TABLE_NAME",
+        "EVENT_BUS_NAME": "EVENT_BUS_NAME",
+        "ORDERS_LIMIT": "20",
+        "POWERTOOLS_TRACE_DISABLED": "true"
+    }
+}])(lambda_module)
+context = pytest.fixture(context)
+
+
+@pytest.fixture(scope="module")
+def order(get_order):
+    return get_order()
+
+
+@pytest.fixture
+def order_products(order):
+    return [
+        {
+            "orderId": order["orderId"],
+            "productId": product["productId"],
+            "quantity": product["quantity"]
+        }
+        for product in order["products"]
+    ]
+
+
+@pytest.fixture
+def ddb_record_metadata_completed(order):
+    return {
+        "eventName": "MODIFY",
+        "eventSource": "aws:dynamodb",
+        "eventVersion": "1",
+        "dynamodb": {
+            "Keys": {
+                "orderId": {"S": order["orderId"]},
+                "productId": {"S": METADATA_KEY}
+            },
+            "NewImage": {
+                "orderId": {"S": order["orderId"]},
+                "productId": {"S": METADATA_KEY},
+                "status": {"S": "COMPLETED"},
+                "modifiedDate": {"S": order["modifiedDate"]}
+            },
+            "OldImage": {
+                "orderId": {"S": order["orderId"]},
+                "productId": {"S": METADATA_KEY},
+                "status": {"S": "IN_PROGRESS"},
+                "modifiedDate": {"S": order["modifiedDate"]}
+            },
+            "SequenceNumber": "123456789012345678901",
+            "SizeBytes": 123,
+            "StreamViewType": "NEW_AND_OLD_IMAGES"
+        }
+    }
+
+@pytest.fixture
+def event_metadata_completed(order, order_products):
+    return {
+        "Source": "ecommerce.warehouse",
+        "DetailType": "PackageCreated",
+        "Resources": [order["orderId"]],
+        "EventBusName": "EVENT_BUS_NAME", 
+        "Detail": json.dumps({
+            "orderId": order["orderId"],
+            "products": order_products
+        })
+    }
+
+
+@pytest.fixture
+def ddb_record_metadata_product(order):
+    return {
+        "eventName": "MODIFY",
+        "eventSource": "aws:dynamodb",
+        "eventVersion": "1",
+        "dynamodb": {
+            "Keys": {
+                "orderId": {"S": order["orderId"]},
+                "productId": {"S": order["products"][0]["productId"]}
+            },
+            "NewImage": {
+                "orderId": {"S": order["orderId"]},
+                "productId": {"S": order["products"][0]["productId"]}
+            },
+            "OldImage": {
+                "orderId": {"S": order["orderId"]},
+                "productId": {"S": order["products"][0]["productId"]}
+            },
+            "SequenceNumber": "123456789012345678901",
+            "SizeBytes": 123,
+            "StreamViewType": "NEW_AND_OLD_IMAGES"
+        }
+    }
+
+
+@pytest.fixture
+def ddb_record_metadata_removed(order):
+    return {
+        "eventName": "REMOVE",
+        "eventSource": "aws:dynamodb",
+        "eventVersion": "1",
+        "dynamodb": {
+            "Keys": {
+                "orderId": {"S": order["orderId"]},
+                "productId": {"S": METADATA_KEY}
+            },
+            "OldImage": {
+                "orderId": {"S": order["orderId"]},
+                "productId": {"S": METADATA_KEY},
+                "status": {"S": "IN_PROGRESS"},
+                "modifiedDate": {"S": order["modifiedDate"]}
+            },
+            "SequenceNumber": "123456789012345678901",
+            "SizeBytes": 123,
+            "StreamViewType": "NEW_AND_OLD_IMAGES"
+        }
+    }
+
+
+def test_get_products(lambda_module, order, order_products):
+    """
+    Test get_products()
+    """
+
+    table = mock_table(
+        lambda_module.table, "query",
+        ["orderId", "productId"],
+        items=order_products
+    )
+
+    response = lambda_module.get_products(order["orderId"])
+
+    table.assert_no_pending_responses()
+    table.deactivate()
+
+    assert response == order_products
+
+
+def test_parse_record_metadata_completed(lambda_module, ddb_record_metadata_completed, event_metadata_completed, order, order_products):
+    """
+    Test parse_record() with a metadata completed item
+    """
+
+    table = mock_table(
+        lambda_module.table, "query",
+        ["orderId", "productId"],
+        items=order_products
+    )
+
+    response = lambda_module.parse_record(ddb_record_metadata_completed)
+
+    table.assert_no_pending_responses()
+    table.deactivate()
+
+    assert response is not None
+    compare_event(event_metadata_completed, response)
+
+
+def test_parse_record_metadata_product(lambda_module, ddb_record_metadata_product):
+    """
+    Test parse_record() with a product  item
+    """
+
+    response = lambda_module.parse_record(ddb_record_metadata_product)
+
+    assert response is None
+
+
+def test_parse_record_metadata_removed(lambda_module, ddb_record_metadata_removed):
+    """
+    Test parse_record() with a metadata removed item
+    """
+
+    response = lambda_module.parse_record(ddb_record_metadata_removed)
+
+    assert response is None
+
+
+def test_handler_metadata_completed(lambda_module, context, ddb_record_metadata_completed, ddb_record_metadata_product, ddb_record_metadata_removed, event_metadata_completed, order, order_products):
+    """
+    Test handler() with a metadata completed item
+    """
+
+    event_metadata_completed = copy.deepcopy(event_metadata_completed)
+
+    table = mock_table(
+        lambda_module.table, "query",
+        ["orderId", "productId"],
+        items=order_products
+    )
+    # Stubbing Event Bridge
+    eventbridge = stub.Stubber(lambda_module.eventbridge)
+    # Ignore time and detail
+    event_metadata_completed["Time"] = stub.ANY
+    event_metadata_completed["Detail"] = stub.ANY
+    expected_params = {"Entries": [event_metadata_completed]}
+    eventbridge.add_response("put_events", {}, expected_params)
+    eventbridge.activate()
+
+    event = {"Records": [
+        ddb_record_metadata_completed,
+        ddb_record_metadata_product,
+        ddb_record_metadata_removed
+    ]}
+
+    lambda_module.handler(event, context)
+
+    table.assert_no_pending_responses()
+    table.deactivate()
