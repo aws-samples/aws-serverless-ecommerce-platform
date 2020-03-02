3e2de22fcbe1d8951b9f4c60bb1e4beb8a303d2f
Nicolas Moutschen <moutschn@amazon.com>
2020-03-02 09:47:22 +0100

    delivery: add TableUpdate and integ tests

---
+++ delivery/tests/integ/test_events.py
@@ -0,0 +1,94 @@
+import json
+import uuid
+import pytest
+import boto3
+from fixtures import listener # pylint: disable=import-error
+from helpers import get_parameter # pylint: disable=import-error,no-name-in-module
+
+
+@pytest.fixture
+def table_name():
+    """
+    DynamoDB table name
+    """
+
+    return get_parameter("/ecommerce/{Environment}/delivery/table/name")
+
+
+@pytest.fixture
+def order():
+    return {
+        "orderId": str(uuid.uuid4()),
+        "address": {
+            "name": "John Doe",
+            "companyName": "Company Inc.",
+            "streetAddress": "123 Street St",
+            "postCode": "12345",
+            "city": "Town",
+            "state": "State",
+            "country": "SE",
+            "phoneNumber": "+123456789"
+        }
+    }
+
+
+def test_table_update_completed(table_name, listener, order):
+    """
+    Test that the TableUpdate function reacts to changes to DynamoDB and sends events to EventBridge
+    """
+
+    table = boto3.resource("dynamodb").Table(table_name) # pylint: disable=no-member
+
+    # Add a new item
+    order["status"] = "IN_PROGRESS"
+    table.put_item(Item=order)
+
+    # Set the item to COMPLETED
+    order["status"] = "COMPLETED"
+    table.put_item(Item=order)
+
+    # Listen for messages on EventBridge through a listener SQS queue
+    messages = listener("delivery")
+
+    # Parse messages
+    found = False
+    for message in messages:
+        print("MESSAGE RECEIVED:", message)
+        body = json.loads(message["Body"])
+        if order["orderId"] in body["resources"]:
+            found = True
+            assert body["detail-type"] == "DeliveryCompleted"
+
+    assert found == True
+
+    # Delete the item
+    table.delete_item(Key={"orderId": order["orderId"]})
+
+
+def test_table_update_failed(table_name, listener, order):
+    """
+    Test that the TableUpdate function reacts to changes to DynamoDB and sends events to EventBridge
+    """
+
+    table = boto3.resource("dynamodb").Table(table_name) # pylint: disable=no-member
+
+    # Add a new item
+    order["status"] = "NEW"
+    table.put_item(Item=order)
+
+    # Delete the item
+    table.delete_item(Key={"orderId": order["orderId"]})
+
+    # Listen for messages on EventBridge through a listener SQS queue
+    messages = listener("delivery")
+
+    # Parse messages
+    found = False
+    for message in messages:
+        print("MESSAGE RECEIVED:", message)
+        body = json.loads(message["Body"])
+        if order["orderId"] in body["resources"]:
+            found = True
+            assert body["detail-type"] == "DeliveryFailed"
+
+    assert found == True
