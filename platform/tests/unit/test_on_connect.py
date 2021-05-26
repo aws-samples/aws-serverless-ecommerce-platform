4c2e071ddd9df499d63e82dd50d5e3c98190365a
Nicolas Moutschen <moutschn@amazon.com>
2021-05-26 10:02:15 +0200

    feat: add archives to EventBridge

--- platform/tests/unit/test_on_connect.py
+++ platform/tests/unit/test_on_connect.py
@@ -38,34 +38,6 @@ def test_store_id(lambda_module):
     table.deactivate()
 
 
-def test_enable_rule(lambda_module):
-    """
-    Test enable_rule()
-    """
-
-    eventbridge = stub.Stubber(lambda_module.eventbridge)
-    eventbridge.add_response("enable_rule", {}, {
-        "Name": "EVENT_RULE_NAME",
-        "EventBusName": "EVENT_BUS_NAME"
-    })
-    eventbridge.add_response(
-        "describe_rule",
-        {
-            "Name": "EVENT_RULE_NAME",
-            "State": "ENABLED",
-            "EventBusName": "EVENT_BUS_NAME"
-        },
-        {"Name": "EVENT_RULE_NAME", "EventBusName": "EVENT_BUS_NAME"},
-    )
-    eventbridge.activate()
-
-    lambda_module.enable_rule()
-
-    eventbridge.assert_no_pending_responses()
-
-    eventbridge.deactivate()
-
-
 def test_handler(monkeypatch, lambda_module, context, apigateway_event):
     """
     Test handler()
@@ -77,8 +49,7 @@ def test_handler(monkeypatch, lambda_module, context, apigateway_event):
     event["requestContext"] = {"connectionId": connection_id}
 
     calls = {
-        "store_id": 0,
-        "enable_rule": 0
+        "store_id": 0
     }
 
     def store_id(connection_id_req: str):
@@ -86,15 +57,10 @@ def test_handler(monkeypatch, lambda_module, context, apigateway_event):
         assert connection_id_req == connection_id
     monkeypatch.setattr(lambda_module, "store_id", store_id)
 
-    def enable_rule():
-        calls["enable_rule"] += 1
-    monkeypatch.setattr(lambda_module, "enable_rule", enable_rule)
-
     result = lambda_module.handler(event, context)
 
     assert result["statusCode"] == 200
     assert calls["store_id"] == 1
-    assert calls["enable_rule"] == 1
 
 
 def test_handler_no_id(monkeypatch, lambda_module, context, apigateway_event):
@@ -107,8 +73,7 @@ def test_handler_no_id(monkeypatch, lambda_module, context, apigateway_event):
     event = apigateway_event()
 
     calls = {
-        "store_id": 0,
-        "enable_rule": 0
+        "store_id": 0
     }
 
     def store_id(connection_id_req: str):
@@ -116,12 +81,7 @@ def test_handler_no_id(monkeypatch, lambda_module, context, apigateway_event):
         assert connection_id_req == connection_id
     monkeypatch.setattr(lambda_module, "store_id", store_id)
 
-    def enable_rule():
-        calls["enable_rule"] += 1
-    monkeypatch.setattr(lambda_module, "enable_rule", enable_rule)
-
     result = lambda_module.handler(event, context)
 
     assert result["statusCode"] == 400
     assert calls["store_id"] == 0
-    assert calls["enable_rule"] == 0
