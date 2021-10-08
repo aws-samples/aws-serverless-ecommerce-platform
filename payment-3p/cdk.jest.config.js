38296a584721dbb0777272d9d511b255e7137b3e
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2021-10-08 10:39:49 +0200

    fix(payment-3p): npm audit

--- payment-3p/cdk.jest.config.js
+++ payment-3p/cdk.jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    "roots": [
-      "<rootDir>/tests/lint/"
-    ],
-    testMatch: [ '**/*.test.ts'],
-    "transform": {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-  }
+  "roots": [
+    "<rootDir>/tests/lint/"
+  ],
+  testMatch: [ '**/*.test.ts'],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+};
